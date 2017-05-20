69b69cdcfc93cac5549e3d2b7dcef2b18974d4fb
Joseph Ribbe <social@coderwelsch.com>
2017-05-20 13:53:14 +0200

    Uses now jquery and better css selectors for better / safer data scraping

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const
 	Fetch = require( "node-fetch" ),
 	JsDom = require( "jsdom" ).JSDOM;
 
-
-// variables
+// constants
 const
 	MAX_CONNECTIONS = 3,
 	DUDEN_SEARCH_URL = "http://www.duden.de/suchen/dudenonline/",
-	RESULT_SELECTOR = "#content > section.block > section.wide";
+	RESULT_SELECTOR = "#content > section.block > section.wide",
+	NUMBER_OF_POSSIBLE_FREQUENCY_BARS = 5;
 
 
 module.exports = class DudenSearchApi {
@@ -25,10 +25,27 @@ module.exports = class DudenSearchApi {
 			Fetch( this.generateSearchUrl( word ) ).then( ( resource ) => {
 				return resource.text();
 			} ).then( ( text ) => {
-				resolve( this.parseResult( text ) );
+				let parsedResult = this.parseSearchResult( text ),
+					matchedWord = this.getMatchingWord( parsedResult,word );
+
+				if ( matchedWord !== null ) {
+					this.getWordDefinition( word, matchedWord.definitionUrl ).then( resolve ).catch( reject );
+				} else {
+					reject( new Error( "Could not find matching word" ) );
+				}
 			} ).catch( ( error ) => {
 				reject( error );
 			} );
+		} );
+	}
+
+	getWordDefinition ( word, url ) {
+		return new Promise( ( resolve, reject ) => {
+			Fetch( url ).then( ( resource ) => {
+				return resource.text();
+			} ).then( ( result ) => {
+				resolve( this.parseWordDefinition( word, result ) );
+			} ).catch( reject );
 		} );
 	}
 
@@ -102,7 +119,80 @@ module.exports = class DudenSearchApi {
 		manageRequests();
 	}
 
-	parseResult ( result ) {
+	getMatchingWord ( searchResults, word ) {
+		for ( let result of searchResults ) {
+			if ( result.text === word ) {
+				return result;
+			}
+		}
+
+		return null;
+	}
+
+	parseWordDefinition ( word, result ) {
+		try {
+			const $ = require( "jquery" )( new JsDom( result ).window );
+
+			let $dom = $( "body" ),
+				$wordClass = $dom.find( "div.entry > div:contains(Wortart) .lexem" ),
+				$frequency = $dom.find( "div.entry > div:contains(Häufigkeit) .lexem" ),
+				$hyphenation = $dom.find( "#block-duden-tiles-0 > div.entry:nth-child(2) .lexem" ),
+				$examples = $dom.find( "#block-duden-tiles-0 > div.entry:nth-child(3) .lexem" ),
+				$meanings = $dom.find( "#block-duden-tiles-1 > ol > li" ),
+				$ancestry = $dom.find( "h2:contains(Herkunft)" ).closest( ".block" ).find( ".lexem" ),
+
+				definition = {
+					word: word,
+					props: {}
+				};
+
+			// word class
+			if ( $wordClass.length ) {
+				let match = $wordClass.text().match( /^([^,]+)(?:,?\W?(\w+))?/ );
+
+				if ( match.length > 1 ) {
+					definition.props.wordClass = match[ 1 ];
+				}
+
+				// genum
+				if ( match.length > 2 && match[ 2 ] ) {
+					definition.props.genum = match[ 2 ];
+				}
+			}
+
+			if ( $frequency.length ) {
+
+			}
+
+			// hyphenation
+			if ( $hyphenation.length ) {
+				definition.props.hyphenation = $hyphenation.text().split( "|" );
+			}
+
+			// examples
+			if ( $examples.length ) {
+				definition.props.example = $examples.html();
+			}
+
+			// meanings
+			if ( $meanings.length ) {
+				definition.props.meanings = Array.from( $meanings ).map( ( item ) => {
+					return $( item ).text().trim();
+				} );
+			}
+
+			// ancestry
+			if ( $ancestry.length ) {
+				definition.props.ancestry = $ancestry.text();
+			}
+
+			return definition;
+		} catch ( error ) {
+			console.log( error );
+		}
+	}
+
+	parseSearchResult ( result ) {
 		let $dom = new JsDom( result ),
 			definitions = [],
 			$results = $dom.window.document.querySelectorAll( RESULT_SELECTOR );
